7d31c1f3fe1bd39509c2da86e9557aa586b79578
AllenHsing3 <allen.hsing@gmail.com>
2020-09-20 22:12:20 -0700

    2nd try

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import Introduction from './components/introduction'
 import About from './components/about'
 import Timeline from './components/timeline'
 
+if(process.env.NODE_ENV === 'production') {
+  //Set static folder:
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 class App extends Component {
   render() {
     return (
